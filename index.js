b4090678b2e9a22ef426994ce33245a294406855
Gincha <40606700+Gincha@users.noreply.github.com>
2018-10-05 11:21:38 +0300

    front

--- index.js
+++ index.js
@@ -1,48 +1,94 @@
-$('#save-URL').click(function(){
-    var data = 
-    {id: $('#input-35').val(), 
-    url: $('#input-36').val()
+$('#save-URL').click(function() {
+
+    var name = $('#input-36').val();
+
+    var data = {
+        id: $('#input-36').val(),
+        url: $('#input-35').val()
     };
+
+    if ($('#input-35').val().match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?%#[\]@!\$&'\(\)\*\+,;=.]+$/gm) == null) {
+
+        alert("Link not valid!");
+
+    } else {
+
+        $.ajax({
+            url: 'http://localhost:8080/links',
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(d) {
+	
+                if (d.result) {
+                    alert("Link shortened! Find it at: localhost:8080/links/" + name);
+
+                } else {
+                    alert("Id already exists. Please choose another Id");
+                }
+            }
+        })
+
+
+    }
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$('#register').click(function() {
+
+    var pass = $('#input-3').val();
+	var pass1 = $('#input-4').val();
+
+    var data = {
+        username: $('#input-1').val(),
+        email: $('#input-2').val(),
+	password: $('#input-3').val(),
+    };
+  
+    if ($('#input-1').val().match(/^[-\w\.\$@\*\!]{3,30}$/gm) == null) {
+
+        alert("Username is not valid!");
+
+    } else {
     
-    $.ajax({
-        url: 'http://localhost:8080',
-        type:"POST",
-        data: JSON.stringify(data),
-        contentType:"application/json; charset=utf-8",
-        dataType:"json",
- 
-      })
-});
+	 if ($('#input-2').val().match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/gm) == null) { 
+    
+	alert("Indalid Email!");
 
-/*
-$("button").click(function(e) {
-    e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "/pages/test/",
-        data: { 
-            id: $(this).val(), // < note use of 'this' here
-            access_token: $("#access_token").val() 
-        },
-        success: function(result) {
-            alert('ok');
-        },
-        error: function(result) {
-            alert('error');
-        }
-    });
-});
+	}else {
+  
+         if (pass.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/gm) == null) {
 
+             alert("Password must contain at least eight characters containing at least one letter and one number!");
 
+         } else {
 
-$(document).on('click', '#tasks li .delete-item', function(){
-    var id= $(this).attr('data-id');
-    $(this).parent().remove();
-    $.ajax({
-        url: 'http://localhost:8080/links/'+id,
-        type:"DELETE",
-        contentType:"application/json; charset=utf-8",
-        dataType:"json",
-      })
-});
-*/
+             if (pass != pass1) {
+                 alert("Passwords must match!");
+             } else {
+                 
+                 $.ajax({
+                     url: 'http://localhost:8080/register',
+                     type: "POST",
+                     data: JSON.stringify(data),
+                     contentType: "application/json; charset=utf-8",
+                     dataType: "json",
+                     success: function (d) {
+                         console.log("rezultats: " + d);
+                         if (d.result) {
+                             alert("Thanks for registering!");
+                             window.location.assign("localhost:8080");
+                         } else {
+                             alert("Registration not successfull!");
+                         }
+                     }
+                 });
+             }
+         }
+			
+		}
+	}
+   }
+);
